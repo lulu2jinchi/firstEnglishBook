ff4db4a5f6d79ca783f6d745efa5b167785319ac
lulu2jinchi <349318806@qq.com>
2026-02-26 00:07:54 +0800

    feat: add viewport meta tag for responsive design in app head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,6 +12,7 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
+      viewport: "width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no",
       link: [
         {
           rel: "stylesheet",
